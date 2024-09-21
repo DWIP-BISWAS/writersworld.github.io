8c30c46938a36d46b27cff9ce8dcd98f77b3dad2
DWIP-BISWAS <97239360+DWIP-BISWAS@users.noreply.github.com>
2024-09-21 14:32:16 +0530

    Update app.js

--- app.js
+++ app.js
@@ -14,24 +14,13 @@ sign_in_btn.addEventListener("click", () => {
 document.querySelector('#login-form').addEventListener('submit', (e) => {
   e.preventDefault();
   localStorage.setItem('loggedIn', 'true');
-  window.location.href = 'index.html';
+  localStorage.removeItem('alreadyRedirected'); 
+  window.location.href = 'home.html';
 });
 
 document.querySelector('#signup-form').addEventListener('submit', (e) => {
   e.preventDefault();
   localStorage.setItem('loggedIn', 'true');
-  window.location.href = 'index.html';
-});
-
-
-const sign_in_btn = document.querySelector("#sign-in-btn");
-const sign_up_btn = document.querySelector("#sign-up-btn");
-const container = document.querySelector(".container");
-
-sign_up_btn.addEventListener("click", () => {
-  container.classList.add("sign-up-mode");
-});
-
-sign_in_btn.addEventListener("click", () => {
-  container.classList.remove("sign-up-mode");
+  localStorage.removeItem('alreadyRedirected'); 
+  window.location.href = 'home.html';
 });
