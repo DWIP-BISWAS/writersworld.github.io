93e0866b1be9398a4c3cce2b88f7179864899972
DWIP-BISWAS <97239360+DWIP-BISWAS@users.noreply.github.com>
2024-10-18 12:18:27 +0530

    Update sb.js

--- sb.js
+++ sb.js
@@ -129,3 +129,16 @@ document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('scrollToTopBtn').style.display = 'none';
 });
     
+window.addEventListener("load", function(){
+    setTimeout(
+        function open(event){
+            document.querySelector(".popup").style.display = "block";
+        },
+        1000
+    )
+});
+
+
+document.querySelector("#close").addEventListener("click", function(){
+    document.querySelector(".popup").style.display = "none";
+});
